6542f5cb1c09d358b0c5e05ba92fa4c509a21e4f
Alex <23082806+ironllama@users.noreply.github.com>
2023-12-18 06:32:38 +0900

    Day17

---
+++ 17b.f90
@@ -0,0 +1,88 @@
+program aoc17b
+    use PriorityQueue
+    implicit none
+
+    type Coords
+        integer :: y, x
+    endtype
+
+    integer, allocatable :: lines(:,:), tempLines(:,:)
+    character(len=200) :: line
+    integer :: status, lineNum = 1, i, k, shortest = 99999
+    type(Coords) :: dirs(4), target
+    type(t_priority_queue) :: pq
+    type(t_queue_item) :: curr, next
+    character(len=20), allocatable :: allHistory(:)
+    character(len=20) :: hKey
+
+    do
+        read (*, '(a)', iostat=status) line
+        if (is_iostat_end(status)) exit
+
+        if (allocated(lines)) then
+            allocate(tempLines(lineNum, len_trim(line)))
+            tempLines(1:lineNum - 1,:) = lines
+            call move_alloc(tempLines, lines)
+        else
+            allocate(lines(lineNum, len_trim(line)))
+        endif
+
+        do i = 1, len_trim(line)
+            read(line(i:i), *) lines(lineNum,i:i)
+        enddo
+        ! print *, "LINE[", lineNum, "]: ", lines(lineNum,1:)
+
+        lineNum = lineNum + 1
+    enddo
+
+    dirs = [Coords(1, 0), Coords(0, 1), Coords(-1, 0), Coords(0, -1)]
+    target = Coords(size(lines, 1), size(lines, 2))
+
+    call insert(pq, t_queue_item(0, [1, 1, 1, 1]))
+
+    do while(pq%num > 0)
+        curr = pull(pq)
+        ! print *, "CURR:", curr%p, curr%val
+
+        write (hKey, '(*(i0,:,","))') curr%val
+        ! print *, "HKEY: ", trim(hKey)
+        if (allocated(allHistory)) then
+            if (any(allHistory == hKey)) then
+                cycle
+            endif
+            allHistory = [allHistory, hKey]
+        else
+            allHistory = [hKey]
+        endif
+
+        if (curr%val(1) == target%y .and. curr%val(2) == target%x) then
+            if (curr%p < shortest) then
+                shortest = curr%p
+                exit
+            endif
+        endif
+
+        do i = 1, size(dirs)
+            if ((dirs(i)%y == curr%val(3) .and. dirs(i)%x == curr%val(4)) &
+                .or. (dirs(i)%y == (-1 * curr%val(3)) .and. dirs(i)%x == (-1 * curr%val(4)))) cycle
+
+            next = curr;
+            next%val(3) = dirs(i)%y
+            next%val(4) = dirs(i)%x
+
+            do k = 1, 10 
+                next%val(1) = next%val(1) + dirs(i)%y
+                next%val(2) = next%val(2) + dirs(i)%x
+                ! print *, "NEXT:", next%val
+                if (next%val(1) > 0 .and. next%val(1) <= size(lines, 1) &
+                    .and. next%val(2) > 0 .and. next%val(2) <= size(lines, 2)) then
+                    next%p = next%p + lines(next%val(1), next%val(2))
+                    if (k > 3) call insert(pq, next)
+                endif
+            enddo
+        enddo
+    enddo
+
+    print *, "PART 2:", shortest  ! Takes about 7 minutes!
+
+end program aoc17b
--- priorityqueue.f90
+++ priorityqueue.f90
@@ -1,10 +1,15 @@
-! Source: https://qiita.com/ue1221/items/b923bb702c6e1a2d3cb2
+! Based on: https://qiita.com/ue1221/items/b923bb702c6e1a2d3cb2
 module PriorityQueue
   implicit none
 
+  type t_queue_item
+    integer :: p
+    integer, allocatable :: val(:)
+  end type t_queue_item
+
   type t_priority_queue
     integer :: num = 0
-    integer, pointer :: heap(:) => null()
+    type(t_queue_item), pointer :: heap(:) => null()
   end type t_priority_queue
 
 contains
@@ -12,9 +17,10 @@ contains
   subroutine insert(pq,item)
     implicit none
     type(t_priority_queue), intent(inout) :: pq
-    integer, intent(in) :: item
-    integer :: n, i, t
-    integer, allocatable :: tmp(:)
+    type(t_queue_item), intent(in) :: item
+    integer :: n, i
+    type(t_queue_item) :: t
+    type(t_queue_item), allocatable :: tmp(:)
 
     if (.not.associated(pq%heap)) allocate(pq%heap(1))
     if (pq%num == size(pq%heap)) then
@@ -32,7 +38,7 @@ contains
     n = pq%num
     do while (n > 1)
       i = n/2
-      if (pq%heap(n) < pq%heap(i)) then
+      if (pq%heap(n)%p < pq%heap(i)%p) then
         t = pq%heap(n)
         pq%heap(n) = pq%heap(i)
         pq%heap(i) = t
@@ -54,7 +60,8 @@ contains
   function pull(pq) result(item)
     implicit none
     type(t_priority_queue), intent(inout) :: pq
-    integer :: item, n, i, j, tmp
+    integer :: n, i, j
+    type(t_queue_item) :: item, tmp
 
     n = pq%num
     item = pq%heap(1)
@@ -64,8 +71,8 @@ contains
     i = 1
     do while (2*i < n)
       j = 2*i
-      if (j+1 < n .and. pq%heap(j+1) < pq%heap(j)) j = j+1
-      if (pq%heap(j) < pq%heap(i)) then
+      if (j+1 < n .and. pq%heap(j+1)%p < pq%heap(j)%p) j = j+1
+      if (pq%heap(j)%p < pq%heap(i)%p) then
         tmp = pq%heap(j)
         pq%heap(j) = pq%heap(i)
         pq%heap(i) = tmp
@@ -78,7 +85,7 @@ contains
   function peek(pq) result(item)
     implicit none
     type(t_priority_queue), intent(inout) :: pq
-    integer :: item
+    type(t_queue_item) :: item
 
     item = pq%heap(1)
     return
